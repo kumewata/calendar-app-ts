57c0360e0732295801f03ce39ec2b45632a38899
Wataru Kume <kumewata@gmail.com>
2020-11-26 22:12:50 +0900

    add share page function

--- src/store/shared-user.ts
+++ src/store/shared-user.ts
@@ -57,3 +57,9 @@ export const update = (profile: Profile) => {
   targetUser.themeColor = profile.themeColor;
   targetUser.hasAvatar = profile.hasAvatar;
 };
+
+export const searchUsers = partOfNickname => {
+  return sharedUserStore.sharedUsers.filter(user =>
+    user.nickname.startsWith(partOfNickname),
+  );
+};
